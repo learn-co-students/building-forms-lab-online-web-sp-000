331c4664d643487a08a27c585a2ee8cf6f257375
tuzmusic <tuzmusic@gmail.com>
2019-03-18 16:57:55 -0400

    cleanup (still one test failing)

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-/* 
-When I use BandInput.handleSubmit, it works in the browser but the tests say:
-     Invariant Violation: Could not find "store" in either the context or props of "Connect(BandInput)". Either wrap the root component in a <Provider>, or explicitly pass "store" as a prop to "Connect(BandInput)".
-When I pass onSubmit into props from BandsContainer, that error is gone,
-but I can't figure out how to affect the container state (to send the band
-  name to the dispatch)
-*/
-
 class BandInput extends Component {
   state = { name: "" };
   // handleSubmit = event => {
@@ -16,9 +8,9 @@ class BandInput extends Component {
   //   this.props.dispatch({ type: "ADD_BAND", band: this.state.name })
   // };
 
-  // handleChange = e => {
-  //   this.setState({ name: e.target.value });
-  // };
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
 
   onTextChange = event => {
     this.setState({ name: event.target.value });
