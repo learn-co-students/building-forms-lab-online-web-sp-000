d4ee7f9f31f5b81501c057f0e8246d06a0da836a
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-06-10 08:54:02 -0400

    Done.

---
+++ src/containers/BandList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const BandList = props => {
+    const bands = props.bands.map((band, index) => {
+      return <li key={index}>{band.name}</li>;
+    });
+  
+    return (
+      <div>
+          Band List:
+        {bands}
+      </div>
+    );
+  
+  };
+
+  export default BandList;
+
