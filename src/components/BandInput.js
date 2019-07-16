1189b992ae64935ccd1dec01c53d6240c2d2f3cd
github username <github email address>
2019-07-16 19:22:23 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,54 @@
 // Add BandInput component
 import React, { Component } from 'react'
+// import {connect} from 'react-redux'
+
 
 class BandInput extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    this.props.addBand(this.state)
+
+    this.setState({
+      name: ''
+    })
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={(event) => {this.handleSubmit(event)}}>
+          Name:<br></br>
+        <input type='text' name='name' onChange={(event) => {this.handleChange(event)}} value={this.state.name}></input><br></br>
+          <input type='submit' value='Submit'></input>
+        </form>
       </div>
     )
   }
 }
+
+
+// //create a JS object that contains a function called addBand. When I call this.props.addBand(<form payload>) elsewhere, it comes back here to run our function
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addBand: (formData) => {
+//       dispatch({type: 'ADD_BAND', payload: formData})
+//     }
+//   }
+// }
 
 export default BandInput
---
+++ src/components/BandList.js
@@ -0,0 +1,10 @@
+//this component needs to access our store to grab our band name that we input and submitted via the form. In other words, this component is on;y concerned with the vaues already inside the store.
+import React from 'react'
+
+const BandList = () => {
+  //map over bands and return an li
+
+    return(<div>BandList</div>)
+}
+
+export default BandList
