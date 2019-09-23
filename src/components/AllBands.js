15ee250277b8d7b9e605023d48ae1fd17a7365e9
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-09-23 15:42:31 +0100

    Done.

---
+++ src/components/AllBands.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AllBands = ({ bands }) => {
+  const renderBands = bands.map((band) => {
+    return <li>{band.name}</li>
+  });
+
+  return (
+    <div>
+      {renderBands}
+    </div>
+  )
+}
+
+export default AllBands;
