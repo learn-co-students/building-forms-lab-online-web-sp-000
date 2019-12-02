a88e807e34aabde70150c963b96679aa18b6e157
milenaonmaui <milenakari@yahoo.com>
2019-12-01 17:54:23 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import BandsContainer from './containers/BandsContainer';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App">        
         <BandsContainer />
       </div>
     );
