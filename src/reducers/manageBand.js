c0f7ca99171ddba93004c46b79aec11b5f29babc
michellemoon236 <michellemoon236@gmail.com>
2020-02-05 11:48:17 -0800

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,27 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  switch (action.type) {
+    case 'ADD_BAND':
+
+      return { bands: state.bands.concat(action.payload.text) };
+
+    default:
+      return state;
+  }
 };
+
+
+
+// export default function manageTodo(state = {
+//   todos: [],
+// }, action) {
+//   switch (action.type) {
+//     case 'ADD_TODO':
+
+//       return { todos: state.todos.concat(action.payload.text) };
+
+//     default:
+//       return state;
+//   }
+// }
