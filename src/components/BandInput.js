b7e1e324c921030fb0962906a7acebc04c571fad
github username <github email address>
2020-06-09 13:11:52 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,56 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class BandInput extends Component {
+
+  state = {
+    name: ''
+  }
+
+  handleChange = event => {
+//debugger;
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addBand(this.state)
+    this.setState({
+      name: ''
+    })
+    //document.getElementById('input-box').value = '';
+  }
+
   render() {
     return(
       <div>
         Band Input
+        <form
+          onSubmit={(event) => this.handleSubmit(event)}>
+          <label>
+            Add band
+          </label>
+          <input
+            id='input-box'
+            type='text'
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.text}>
+          </input>
+          <input
+            type='submit'>
+          </input>
+        </form>
       </div>
     )
   }
 }
 
-export default BandInput
+
+
+export default BandInput;
+
+
+// You will need to write the BandInput component as a child of BandsContainer. This component should have a form with a text input and submit button. We would like to update this component's state each time the user types a new character in the input, stored as this.state.name. We will only update the Redux store's state when the user hits the submit button. The dispatched action required to initiate this update should be set up in BandsContainer and passed down as the prop addBand to BandInput.
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import manageBand from '../reducers/manageBand';
+import BandInput from '../components/BandInput';
+
+//const renderBands = () => props.manageBand.bands.map((band, id) => <li key={id}>{band}</li>);
+
+//1.define as an action creator: type and data. Creates an action based on data, and returns it.
+const addBandActionCreator = (bandObject) => {
+  console.log(bandObject);
+  console.log({type: "ADD_BAND", band: bandObject});
+  return {type: "ADD_BAND", band: bandObject}
+}
+
+
 
 class BandsContainer extends Component {
+
+//renderBands = (state) => state.bands.map((band) => <li>{band}</li>);
+
   render() {
+//    console.log('here');
     return(
       <div>
-        BandsContainer
+        <div>
+            <BandInput addBand={this.props.addBand}/>
+        </div>
+        <div>
+            BandsContainer
+            <ul id="band-container-list">
+              
+            </ul>
+        </div>
       </div>
     )
   }
 }
+//{this.renderBands()}
 
-export default BandsContainer
+
+
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+//2. map the action creator to props. Dispatch always expects an object {type: , someData: }, and not even really someData.
+const mapDispatchToProps = dispatch => {
+  return {addBand: (stateObject) => dispatch(addBandActionCreator(stateObject))}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
+//export default connect (BandsContainer);
+
+// This component should act as your connection to the Redux store and should be a child of the App component. Any values we want to use from Redux as well as any actions we want to dispatch should be set up and available in BandsContainer so they can be passed down to other components.
