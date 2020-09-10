0f795b7033decad7006c6dcc6240f34503dc63cb
juan siu <juan.siu@gmail.com>
2020-09-10 13:24:37 +0000

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,12 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  console.log('bands reducer', state, action)
+  switch (action.type) {
+    case 'ADD_BAND':
+      return { bands: state.bands.concat(action.payload) };
+
+    default:
+      return state;
+  }
 };
