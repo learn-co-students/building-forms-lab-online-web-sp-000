ae4d37488a8cafe2adacaf028928c865823b0317
connorworthen <connorw220@protonmail.com>
2020-12-19 01:09:32 -0800

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,46 @@
-// Add BandInput component
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addBand } from '../actions'
 
 class BandInput extends Component {
+
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  state = {
+    name: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    debugger
+    this.props.addBand(this.state)
+
+    this.setState({
+      name: ''
+    })
+  }
+
   render() {
     return(
       <div>
         Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' name="name" value={this.state.name} onChange={this.handleChange}></input>
+          <input type='submit'></input>
+        </form>
+        
       </div>
     )
   }
-}
+};
 
 export default BandInput
---
+++ src/components/bands.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Bands extends React.Component {
+
+  render() {
+    let bands = this.props.bands.map((band, index) => <li key={index}>{band.name}</li>)
+
+    return (
+      <ul>
+        {bands}
+      </ul>
+    )
+  }
+}
