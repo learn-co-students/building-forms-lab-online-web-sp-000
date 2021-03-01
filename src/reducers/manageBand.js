8a8cd2f6699ad7193a6b1d59949acdbd9a34e383
Emily Gann <aspentree79@gmail.com>
2021-02-28 20:15:16 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,15 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
-};
+
+  switch (action.type) {
+    case 'ADD_BAND':
+      return {...state, bands: [...state.bands, action.band]}
+      // reducers take in state + action to return a new global state for components to use
+      // should concatenate a new band each time the action is dispatched -- ??
+      
+      default:
+      return state;
+  }
+
+}
