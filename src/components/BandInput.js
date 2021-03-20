29869301c77095a6296a0a2182d52775b5099673
ismayton <ian.mayton@gmail.com>
2021-03-20 18:54:04 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,35 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addBand(this.state.name)
+    this.setState({
+      name: ''
+    })
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} value={this.state.name}></input>
+          <input type="submit"/>
+        </form>
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
+  
+  addBand = (name) => {
+    this.props.dispatch({
+      type: "ADD_BAND",
+      name: name
+    })
+  }
+
+  renderBands = () => {
+    return this.props.bands.map(band => <li>{band.name}</li>)
+  }
+
   render() {
     return(
       <div>
         BandsContainer
+      <BandInput addBand={this.addBand} />
+      <ul>
+        {this.renderBands()}
+      </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {bands: state.bands} 
+}
+
+export default connect(mapStateToProps)(BandsContainer)
