c2cfa9cd4c52c1a32a310e1fb2bc71755e5ecedf
catprice444 <catprice444@gmail.com>
2021-05-26 06:42:30 -0400

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import BandInput from '../components/BandInput.js';
+import Bands from '../components/Bands.js';
+import {connect} from 'react-redux';
 
 class BandsContainer extends Component {
   render() {
+    // debugger
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand} />
+        <Bands bands={this.props.bands} />
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = ({bands}) => ({bands})
+
+const mapDispatchToProps = dispatch => ({
+  addBand: (band) => dispatch({ type: "ADD_BAND", band })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+
+// 1. You will need to write the `BandsContainer` component. This component should
+//    act as your connection to the Redux store and should be a child of the App
+//    component. Any values we want to use from Redux as well as any actions we
+//    want to dispatch should be set up and available in `BandsContainer` so they
+//    can be passed down to other components.
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,15 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
-};
+  switch (action.type) {
+    case "ADD_BAND":
+      return {...state, bands: [...state.bands, action.band] };
+    default:
+      return state;
+  }
+}
+
+// 3. You will also need to fill in the `manageBand` reducer located under
+//    `./src/reducers/manageBand.js`. It will need to respond to an action of `type
+//    'ADD_BAND'` and correspondingly concatenate a new band each time the action
+//    is dispatched.
