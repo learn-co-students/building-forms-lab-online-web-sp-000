5a3802b042ad4970d81813b38468c7d463b72b7c
Logan Smith <logsmith06@gmail.com>
2021-06-13 20:39:25 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import BandsContainer from './containers/BandsContainer';
 
+import {connect} from 'react-redux';
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,5 +14,9 @@ class App extends Component {
     );
   }
 };
+const mapStateToProps = (state) => {
+  // debugger;
+  return { items: state.items }
+}
 
-export default App;
+export default App
--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,34 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
 
 class BandInput extends Component {
+
+state = {
+  name: ''
+}
+  handleChange(event) { this.setState({name: event.target.value}) }
+  handleOnSubmit(event)  {
+    event.preventDefault();
+    this.props.addBand(this.state);
+
+    this.setState({
+      name: '',
+    })
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input onChange={(event) => this.handleChange(event)}type="text" id="name" value={this.state.name} />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
+
 
 export default BandInput
