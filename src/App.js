bbcaf8d131cb462e6d8b92eca622b96d068142a4
Kiana Long <Kianajade42.kl@gmail.com>
2021-06-18 16:29:44 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import BandsContainer from './containers/BandsContainer';
+import React, { Component } from 'react'
+import BandsContainer from './containers/BandsContainer'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BandsContainer />
+       
       </div>
     );
   }
